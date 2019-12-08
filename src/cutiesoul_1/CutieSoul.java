5e0e58647401f126b79c4241c7067bcb66ed55c0
jiyoung seo <kiki606a@gmail.com>
2019-12-08 19:10:53 +0900

    리듬게임 만들기
    자바로 이용한 리듬게임 만들기 개인프로젝트 실행중...

---
+++ src/cutiesoul_1/CutieSoul.java
@@ -0,0 +1,15 @@
+package cutiesoul_1;
+
+import javax.swing.JFrame;
+
+public class CutieSoul extends JFrame {
+
+	public CutieSoul() {
+		setTitle("CutieSoul");
+		setSize(Main.SCREEN_WIDTH,Main.SCREEN_HEIGHT);
+		setResizable(false);
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setVisible(true);
+	}
+}
